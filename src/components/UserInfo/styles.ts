b7c78a81f94d44da9497b0d961d7ea9fbef06b98
Rodrigo Xavier <rodrfx@gmail.com>
2022-06-02 13:30:28 -0300

    feat: stlyes component UserInfo

--- src/components/UserInfo/styles.ts
+++ src/components/UserInfo/styles.ts
@@ -1,3 +1,4 @@
+import { shade } from 'polished';
 import styled from 'styled-components';
 
 export const Container = styled.main`
@@ -25,6 +26,7 @@ export const Container = styled.main`
 
 	> p {
 		margin: 2rem 0;
+		width: 90%;
 	}
 
 	div {
@@ -54,6 +56,7 @@ export const Container = styled.main`
 			padding: 1rem;
 			cursor: pointer;
 			transition: color 0.2s;
+			transition: background-color 0.2s;
 
 			p:nth-child(1) {
 				font-weight: 600;
@@ -62,8 +65,20 @@ export const Container = styled.main`
 
 			&:hover {
 				color: ${(props) => props.theme.colors.secondary};
+				background-color: ${(props) => shade(0.3, props.theme.colors.primary)};
+				border-radius: 10px;
+				/* opacity: 0.5; */
 			}
 		}
+	}
+
+	span {
+		position: absolute;
+		bottom: 0;
+		background-color: ${(props) => props.theme.colors.secondary};
+		width: 100%;
+		height: 10px;
+		border-radius: 0 0 10px 10px;
 	}
 
 	@media (min-width: 768px) and (max-width: 1024px) {
