8700a307a6d97a118796c61203635b01f9f0b538
Rodrigo Xavier <rodrfx@gmail.com>
2022-06-02 13:28:12 -0300

    feat(App): add  hook useContext - UserContextProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { DefaultTheme, ThemeProvider } from 'styled-components';
 import { Header } from './components/Header';
 import { InputBox } from './components/InputBox';
 import { UserInfo } from './components/UserInfo';
+import { UserContextProvider } from './context/UserContext';
 import usePersistedState from './hooks/usePersistedState';
 import { GlobalStyle } from './styles/global';
 import dark from './styles/themes/dark';
@@ -15,12 +16,14 @@ const App = () => {
 	};
 
 	return (
-		<ThemeProvider theme={theme}>
-			<GlobalStyle />
-			<Header toggleTheme={toggleTheme} />
-			<InputBox />
-			<UserInfo />
-		</ThemeProvider>
+		<UserContextProvider>
+			<ThemeProvider theme={theme}>
+				<GlobalStyle />
+				<Header toggleTheme={toggleTheme} />
+				<InputBox />
+				<UserInfo />
+			</ThemeProvider>
+		</UserContextProvider>
 	);
 };
 
