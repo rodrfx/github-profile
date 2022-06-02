e936b12f163e334a4fb00b037ac9554b798abf93
Rodrigo Xavier <rodrfx@gmail.com>
2022-06-02 13:29:43 -0300

    feat(components/UseeInfo): add component

--- src/components/UserInfo/index.tsx
+++ src/components/UserInfo/index.tsx
@@ -1,29 +1,25 @@
+import { useContext, useState } from 'react';
+import { UserContext } from '../../context/UserContext';
 import * as S from './styles';
+import { UserNumbers } from './UserNumbers';
+import { UserPerfil } from './UserPerfil';
 
 export const UserInfo: React.FC = () => {
+	const { userData } = useContext(UserContext);
+	const [isShowRepository, setIsShowRepository] = useState(false);
+
 	return (
 		<S.Container>
-			<img
-				src="https://avatars.githubusercontent.com/u/68661446?s=400&u=40e07c8a8b71b8d64c31a5419e97251884c5a186&v=4"
-				alt="foto de perfil"
-			/>
-
-			<h2>Rodrigo Xavier</h2>
-			<p>Descrição do perfil</p>
-			<div>
-				<section>
-					<p>4</p>
-					<p>Repositórios</p>
-				</section>
-				<section>
-					<p>10</p>
-					<p>Seguidores</p>
-				</section>
-				<section>
-					<p>1</p>
-					<p>Seguindo</p>
-				</section>
-			</div>
+			{isShowRepository ? (
+				<div>REPOSITORIO</div>
+			) : userData.name ? (
+				<>
+					<UserPerfil userData={userData} />
+					<UserNumbers userData={userData} />
+				</>
+			) : (
+				<span />
+			)}
 		</S.Container>
 	);
 };
