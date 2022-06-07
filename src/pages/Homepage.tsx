bf69b58d70c76868805c238cc96169e516c459f6
Rodrigo Xavier <rodrfx@gmail.com>
2022-06-07 16:54:03 -0300

    feat(pages/Homepage): add homepage

---
+++ src/pages/Homepage.tsx
@@ -0,0 +1,11 @@
+import { InputBox } from '../components/InputBox';
+import { UserInfo } from '../components/UserInfo';
+
+export const Homepage = () => {
+	return (
+		<>
+			<InputBox />
+			<UserInfo />
+		</>
+	);
+};
