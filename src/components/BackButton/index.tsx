7d0cd2f23993aa23234544fe33309a626ce0c828
Rodrigo Xavier <rodrfx@gmail.com>
2022-06-09 10:25:18 -0300

    feat(components/BackButton): added styled className

--- src/components/BackButton/index.tsx
+++ src/components/BackButton/index.tsx
@@ -2,11 +2,11 @@ import * as S from './styles';
 
 export const BackButton = () => {
 	return (
-		<button type="button">
+		<S.BackBtn type="button">
 			<S.LinkBtn to={'/'}>
 				<S.ArrowBtn size={18} />
 				Voltar
 			</S.LinkBtn>
-		</button>
+		</S.BackBtn>
 	);
 };
