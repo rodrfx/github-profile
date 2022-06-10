f48fdbb9958425d8dd2231312eddf95d8206c141
Rodrigo Xavier <rodrfx@gmail.com>
2022-06-10 10:22:26 -0300

    feat(services/following): typing in the function and switching console.log to capture the api response

--- src/services/following.ts
+++ src/services/following.ts
@@ -1,12 +1,13 @@
+import { Following } from '../types/following';
 import { api } from './api';
 
 export const getUserFollowing = async (
 	username: string,
-	setUserFollowing: () => void
+	setUserFollowing: (following: Following[]) => void
 ) => {
 	try {
 		const response = await api.get(`${username}/following`);
-		console.log(response.data);
+		setUserFollowing(response.data);
 	} catch (error) {
 		console.error(error);
 	}
