bace3f685ca8ddefcdc8a98dccae4f1534f2a6f3
Rodrigo Xavier <rodrfx@gmail.com>
2022-06-10 14:52:56 -0300

    feat(src/routes): added routes for following and followers

--- src/routes/Coordinator.tsx
+++ src/routes/Coordinator.tsx
@@ -1,4 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
+import { FollowersPage } from '../pages/FollowersPage';
+import { FollowingPage } from '../pages/FollowingPage';
 import { Homepage } from '../pages/Homepage';
 import { RepositoriesPage } from '../pages/RepositoriesPage';
 
@@ -7,6 +9,8 @@ export const Coordinator = () => {
 		<Routes>
 			<Route path="/" element={<Homepage />} />
 			<Route path="/repositories" element={<RepositoriesPage />} />
+			<Route path="/followers" element={<FollowersPage />} />
+			<Route path="/following" element={<FollowingPage />} />
 		</Routes>
 	);
 };
