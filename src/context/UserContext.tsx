db33098eb3e70b2a6a4ba47ad2eaa2d3a41d6cfe
Rodrigo Xavier <rodrfx@gmail.com>
2022-06-10 15:09:26 -0300

    feat(context/UserContext): added userFollowers and userFollowing states to the context

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,4 +1,6 @@
 import { createContext, ReactNode, useState } from 'react';
+import { Follower } from '../types/follower';
+import { Following } from '../types/following';
 import { Repository } from '../types/repository';
 import { User } from '../types/user';
 
@@ -11,6 +13,10 @@ type UserContextProps = {
 	setUserData: (user: User) => void;
 	userRepositories: Repository[];
 	setUserRepositories: (repository: Repository[]) => void;
+	userFollowers: Follower[];
+	setUserFollowers: (follower: Follower[]) => void;
+	userFollowing: Following[];
+	setUserFollowing: (Following: Following[]) => void;
 };
 
 export const UserContext = createContext<UserContextProps>(
@@ -20,10 +26,21 @@ export const UserContext = createContext<UserContextProps>(
 export const UserContextProvider = ({ children }: UserContextProviderProps) => {
 	const [userData, setUserData] = useState({} as User);
 	const [userRepositories, setUserRepositories] = useState([] as Repository[]);
+	const [userFollowers, setUserFollowers] = useState([] as Follower[]);
+	const [userFollowing, setUserFollowing] = useState([] as Following[]);
 
 	return (
 		<UserContext.Provider
-			value={{ userData, userRepositories, setUserData, setUserRepositories }}
+			value={{
+				userData,
+				userRepositories,
+				setUserData,
+				setUserRepositories,
+				userFollowers,
+				setUserFollowers,
+				userFollowing,
+				setUserFollowing,
+			}}
 		>
 			{children}
 		</UserContext.Provider>
